df75e2b09a653e16eccb1dae28c8c6c3106beb16
Felipe Gomes <felipe_gomessantos@hotmail.com>
2015-09-08 09:43:50 -0300

    Create ManadaVirgula.java

---
+++ hierarquia/ManadaVirgula.java
@@ -0,0 +1,12 @@
+package hierarquia;
+public class ManadaVirgula extends Manada {
+String respectiva="";
+@Override
+public String estourar() {
+for(Animal a:animais)
+{
+respectiva+=a.fazerBarulho()+",";
+}
+return respectiva;
+}
+}

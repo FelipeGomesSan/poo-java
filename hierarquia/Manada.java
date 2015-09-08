f874a1457369dec367325975e144d24d3d648134
Felipe Gomes <felipe_gomessantos@hotmail.com>
2015-09-08 09:42:56 -0300

    Create Manada.java

---
+++ hierarquia/Manada.java
@@ -0,0 +1,11 @@
+package hierarquia;
+import java.util.List;
+import java.util.LinkedList;
+public abstract class Manada {
+protected List<Animal>animais=new LinkedList<Animal>();
+public abstract String estourar();
+public void adicionarAnimal(Animal animal)
+{
+animais.add(animal);
+}
+}

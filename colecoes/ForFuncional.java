e463fc5255bf06f327c1480cacfcd370c9b01ba8
Felipe Gomes <felipe_gomessantos@hotmail.com>
2015-09-30 08:28:34 -0300

    Update ForFuncional.java

--- colecoes/ForFuncional.java
+++ colecoes/ForFuncional.java
@@ -3,6 +3,8 @@ package colecoes;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
 public class ForFuncional {
 	
 	public static void main(String[] args){
@@ -10,14 +12,20 @@ public class ForFuncional {
 		numeros.add(0);
 		numeros.add(1);
 		numeros.add(2);
+		numeros.add(3);
+		numeros.add(4);
+		numeros.add(5);
 		
 	//	for(int n: numeros){
 		//	System.out.println(n);
 	//	}
 		
-	//Consumer<Integer> cons = (Integer n) -> System.out.println(n);
-	//numeros.forEach((Integer n) -> System.out.println(n));
-		numeros.forEach(System.out::println);
+	   //Consumer<Integer> cons = (Integer n) -> System.out.println(n);
+	   //numeros.forEach((Integer n) -> System.out.println(n));
+	   //numeros.stream().filter((Integer n) -> n<3).forEach(System.out::println);
+		numeros.stream().filter((Integer n) -> n<3).map((Integer n) -> n*n).forEach(System.out::println);
 		
 	}
+
+
 }

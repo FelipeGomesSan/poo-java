4f1d22b8e1947a4e7d3a72b32e18c62dc5190c48
Felipe Gomes <felipe_gomessantos@hotmail.com>
2015-10-14 07:55:50 -0300

    Create StringMain.java

---
+++ strings/StringMain.java
@@ -0,0 +1,15 @@
+package strings;
+public class StringMain {
+	public static void main(String[] args){
+		String gomes ='Gomes';
+		String gomes2="Gomes";
+		String gomesNew=new String("Gomes");
+		
+		System.out.println(gomes==gomes2);
+		System.out.println(gomes==gomesNew);
+		System.out.println(gomes.equals(gomes2));
+		System.out.println(gomes.equals(gomesNew));
+		
+	}
+
+}

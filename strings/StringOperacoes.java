ba12dc02f904db6f04a19b0e87085502ef84d5e7
Felipe Gomes <felipe_gomessantos@hotmail.com>
2015-10-14 08:23:50 -0300

    Update StringOperacoes.java

--- strings/StringOperacoes.java
+++ strings/StringOperacoes.java
@@ -3,6 +3,7 @@ package strings;
 public class StringOperacoes {
 	public static void main(String[] args){
 		String g="Gomes";
+		StringBuilder builder=new StringBuilder(g);
 		
 		for(char i='A'; i<'Z';i++){
 			g+=1;
@@ -10,7 +11,11 @@ public class StringOperacoes {
 			
 		}
 		
+		g=builder.toString();
+		
 		System.out.println("String realmente utilizado: "+g);
+		System.out.println(builder.delete(6, 9));
+		System.out.println(builder.insert(6, "BCD"));
 	}
 
 }

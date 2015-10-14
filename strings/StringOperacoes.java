8efcf264a909520c1e9c22841dd2bfe7a63b02fd
Felipe Gomes <felipe_gomessantos@hotmail.com>
2015-10-14 08:13:35 -0300

    Create StringOperacoes.java

---
+++ strings/StringOperacoes.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class StringOperacoes {
+	public static void main(String[] args){
+		String g="Gomes";
+		
+		for(char i='A'; i<'Z';i++){
+			g+=1;
+			System.out.println(g);
+			
+		}
+		
+		System.out.println("String realmente utilizado: "+g);
+	}
+
+}
